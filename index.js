4fa707fce470cdc8165a2152f8dd2abef367c196
asafuch <asafuch@gmail.com>
2021-06-08 19:45:18 +0300

    l

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) =>{
 app.post("/",async (req,res)=>{
     console.log("log start");
     console.log("----------------------------------");
-    console.log(JSON.parse(JSON.stringify(req.body)));
+    console.log(JSON.parse(JSON.stringify(req.body.repository)));
     res.json(req.body)
     console.log("----------------------------------");
     console.log("log ends");
