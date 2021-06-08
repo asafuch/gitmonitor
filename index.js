6fa9d9c229c181d5ee32ef8cf66687cc85521681
asafuch <asafuch@gmail.com>
2021-06-08 19:36:19 +0300

    k

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.post("/",async (req,res)=>{
     console.log("log start");
     console.log("----------------------------------");
     console.log(req.body);
-    res.send(req.body)
+    res.json(req.body)
     console.log("----------------------------------");
     console.log("log ends");
     
