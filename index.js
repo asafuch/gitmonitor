d5ed9c8e65d64b6d6d1bee6bef02fa54ff8cb2e2
asafuch <asafuch@gmail.com>
2021-06-08 19:24:04 +0300

    k

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require("express")
 const bodyParser=require("body-parser")
 const app=express()
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.get('/', (req, res) =>{
     res.send('Hello World! v2') 
     console.log("hello world");
